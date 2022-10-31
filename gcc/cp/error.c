7a893b5da3eb719dc4a41d27fceddbaa1798578b
Zopolis4 <creatorsmithmdt@gmail.com>
2022-10-31 12:01:42 +1100

    Revert "extend.texi (Java Exceptions): Remove."
    This reverts commit bfecd57cd46ac368213f55fc4a3ff67c8c59c5ea.

--- gcc/cp/error.c
+++ gcc/cp/error.c
@@ -3257,6 +3257,9 @@ language_to_string (enum languages c)
     case lang_cplusplus:
       return "C++";
 
+    case lang_java:
+      return "Java";
+
     default:
       gcc_unreachable ();
     }
