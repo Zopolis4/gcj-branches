4db9653bd05645a91940e46e72312a47317149ee
Zopolis4 <creatorsmithmdt@gmail.com>
2022-11-07 20:48:01 +1100

    well well well lettuce

--- gcc/tree.h
+++ gcc/tree.h
@@ -2122,7 +2122,7 @@ extern machine_mode vector_type_mode (const_tree);
 #define TYPE_MAX_VALUE_RAW(NODE) (TYPE_CHECK (NODE)->type_non_common.maxval)
 /* For record and union types, information about this type, as a base type
    for itself.  */
-#define TYPE_BINFO(NODE) (RECORD_OR_UNION_CHECK (NODE)->type_non_common.lang_1)
+#define TYPE_BINFO(NODE) (RECORD_OR_UNION_CHECK (NODE)->type_non_common.binfo)
 
 /* For types, used in a language-dependent way.  */
 #define TYPE_LANG_SLOT_1(NODE) \
