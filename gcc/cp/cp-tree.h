6590a71f6bb8d32d9c3536708bc635a6364f2907
Zopolis4 <creatorsmithmdt@gmail.com>
2022-11-08 12:00:51 +1100

    yeah we really shouldnt have just deleted ptrmemfunc

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -2362,6 +2362,7 @@ struct GTY(()) lang_type {
   unsigned java_interface : 1;
   unsigned debug_requested : 1;
   unsigned fields_readonly : 1;
+  unsigned ptrmemfunc_flag : 1;
 
   unsigned lazy_default_ctor : 1;
   unsigned lazy_copy_ctor : 1;
@@ -2394,7 +2395,7 @@ struct GTY(()) lang_type {
   /* There are some bits left to fill out a 32-bit word.  Keep track
      of this by updating the size of this bitfield whenever you add or
      remove a flag.  */
-  unsigned dummy : 2;
+  unsigned dummy : 1;
 
   tree primary_base;
   vec<tree_pair_s, va_gc> *vcall_indices;
